1470db389c77265da48a3a880b01ec01ef20b7d7
Yash Nandwanii <yashnandwani47@gmail.com>
2024-07-28 11:40:07 +0530

    Day 12 Completed.

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,21 +1,29 @@
-class Item{
-  final String id;
+class CatalogModel {
+  static final products = [
+    Item(
+        id: 1,
+        name: "Iphone 12Pro",
+        description: "iphone ",
+        price: 1299,
+        color: "white",
+        image:
+            "https://imgs.search.brave.com/94j4PNijz11vtJhBTDvEvB2JVeOdHeMorOEB8o_3Wt0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/OTEtaW1nLmNvbS9n/YWxsZXJ5X2ltYWdl/c191cGxvYWRzLzcv/OS83OWQ2ZTVhZDQ5/MzQ1ZTk4NjE4Yzhl/MDA4MzM0YjRlNWNm/NDg0NmE5LmpwZz90/cj1oLTU1MCx3LTAs/Yy1hdF9tYXg")
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String description;
   final int price;
   final String color;
   final String image;
 
-  Item({required this.id, required this.name, required this.description, required this.price, required this.color, required this.image});
-  
-  
+  Item(
+      {required this.id,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.color,
+      required this.image});
 }
-
-final products = [Item(id: "a1",
- name: "Iphone 12Pro",
-  description: "iphone ",
-   price: 1299,
-    color: "white",
-    image: "https://imgs.search.brave.com/94j4PNijz11vtJhBTDvEvB2JVeOdHeMorOEB8o_3Wt0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/OTEtaW1nLmNvbS9n/YWxsZXJ5X2ltYWdl/c191cGxvYWRzLzcv/OS83OWQ2ZTVhZDQ5/MzQ1ZTk4NjE4Yzhl/MDA4MzM0YjRlNWNm/NDg0NmE5LmpwZz90/cj1oLTU1MCx3LTAs/Yy1hdF9tYXg"
-    )
-    ];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,16 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/items.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const int days = 30;
-    const String name = "Codepur";
+    final dummyList = List.generate(40, (index) => CatalogModel.products[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -19,11 +20,16 @@ class HomePage extends StatelessWidget {
             color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
         backgroundColor: Colors.white,
       ),
-      body: Center(
-        child: Container(
-          child: const Text("Welcome, to $days of flutter by $name"),
-        ),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            },
+          )),
       drawer: MyDrawer(),
     );
   }
